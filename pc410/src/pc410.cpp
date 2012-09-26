9c2ab1bd500d259f7d326a1bb5b15a8392020def
jmckay858 <sdstat@gmail.com>
2012-09-26 16:24:34 -0700

    Second commit with fixes

--- pc410/src/pc410.cpp
+++ pc410/src/pc410.cpp
@@ -10,38 +10,42 @@
 using namespace std;
 
 int main() {
-	double discount
+	double discount = 0.0
 		  ,total
 		  ,subtotal;
 
 	int quantity;
 
 	cout << "\n   "
-		 << "How many items did the customer buy?";
+		 << "How many items did the customer buy? ";
 
 	cin >> quantity;
-	if (quantity <= 0)
-
-		cout << "\n   "
-			 << "You need to enter a number greater than"
-			 << "\n0 please run program again.";
-
-	else if (quantity > 10 || quantity < 19)
-
-		discount = .20;
-		else if (quantity > 20 || quantity < 49)
-			discount = .30;
-
-		else if (quantity > 50 || quantity < 99)
-			discount = .40;
-		else
-			discount = .50;
-
+	if (quantity > 0)
+	{
+		 if (quantity > 0 || quantity < 102
+				 )
+			 	 discount = .00;
+		 else if (quantity > 10 || quantity < 19)
+			 	 discount = .20;
+		 else if (quantity > 20 || quantity < 49)
+			 	 discount = .30;
+		 else if (quantity > 50 || quantity < 99)
+			 	 discount = .40;
+		 else
+			 	 discount = .50;
 
 		subtotal = quantity * 99;
-		total = subtotal - (total *discount);
-
+		discount *= subtotal;
+		total = subtotal - discount;
 		cout << total;
+	}
+		else
+		{
+			cout << "\n   "
+				 << "You need to enter a quantity greater\n"
+				 << "   "
+				 << "than 0 please rerun the program.";
+		}
 
 	return 0;
 }
